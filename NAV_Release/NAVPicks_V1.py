86b0f4d9f3d91faa46b29644d97246afeb097966
simplybanana <63216318+simplybanana@users.noreply.github.com>
2020-10-03 07:38:04 -0700

    Add files via upload

--- NAV_Release/NAVPicks_V1.py
+++ NAV_Release/NAVPicks_V1.py
@@ -9,8 +9,7 @@ from time import sleep
 import keyboard
 
 timeInterval = 0.5
-innerLoop = 1
-outerLoop = 1
+innerLoop = False
 counter = 0
 
 winEditSalesOrder = gw.getWindowsWithTitle('Edit - Sales')[0]
@@ -18,192 +17,190 @@ winEditSalesOrder.activate()
 
 
 def waitForWindow(windowName):
-	toc = 0
-	i = 0
-	while i == 0 and toc < 10:
-		try:
-			win = gw.getWindowsWithTitle(windowName)[0]
-			print('Opened after: '+str(toc)+'s')
-			i += 1
-			return True
-		except:
-			print('Not open, waiting for '+windowName+'...')
-		toc = time.perf_counter()
-		sleep(1)
-	sleep(1)
+    toc = 0
+    i = 0
+    while i == 0 and toc < 10:
+        try:
+            win = gw.getWindowsWithTitle(windowName)[0]
+            print('Opened after: '+str(toc)+'s')
+            i += 1
+            return True
+        except:
+            print('Not open, waiting for '+windowName+'...')
+        toc = time.perf_counter()
+        sleep(1)
+    sleep(1)
 
 
 def lookAndPress(imageName):
-	pos = imagesearch(imageName)
-	if pos[0] != -1:
-		pyautogui.click(pos[0],pos[1])
-	else:
-		print("Could not find: " + imageName)
+    pos = imagesearch(imageName)
+    if pos[0] != -1:
+        pyautogui.click(pos[0],pos[1])
+    else:
+        print("Could not find: " + imageName)
 
 
 def lookImageCheck(imageName):
-	pos = imagesearch(imageName)
-	if pos[0] != -1:
-		return True
-	else:
-		return False
+    pos = imagesearch(imageName)
+    if pos[0] != -1:
+        return True
+    else:
+        return False
 
 
 def waitAndPress(imageName):
-	pos = imagesearch_loop(imageName, 0.2)
-	if pos[0] != -1:
-		pyautogui.click(pos[0],pos[1])
-	else:
-		print("Could not find: " + imageName)
+    pos = imagesearch_loop(imageName, 0.2)
+    if pos[0] != -1:
+        pyautogui.click(pos[0],pos[1])
+    else:
+        print("Could not find: " + imageName)
 
 
 def waitImageCheck(imageName):
-	pos = imagesearch_loop(imageName, 0.2)
-	if pos[0] != -1:
-		return True
-	else:
-		print("Could not find: " + imageName)
-		return False
+    pos = imagesearch_loop(imageName, 0.2)
+    if pos[0] != -1:
+        return True
+    else:
+        print("Could not find: " + imageName)
+        return False
 
 
 def homeTab():
-	sleep(timeInterval)
-	pyautogui.press('alt')
-	sleep(timeInterval)
-	pyautogui.press('h')
-	sleep(timeInterval)
+    sleep(timeInterval)
+    pyautogui.press('alt')
+    sleep(timeInterval)
+    pyautogui.press('h')
+    sleep(timeInterval)
 
 
 def navigateTab():
-	pyautogui.press('alt')
-	sleep(timeInterval)
-	pyautogui.press('n')
-	sleep(timeInterval)
+    pyautogui.press('alt')
+    sleep(timeInterval)
+    pyautogui.press('n')
+    sleep(timeInterval)
 
 
 def releaseButton():
-	pyautogui.hotkey('ctrl', 'f9')
+    pyautogui.hotkey('ctrl', 'f9')
 
 
 def createWareShipButton():
-	homeTab()
-	pyautogui.press('w')
+    homeTab()
+    pyautogui.press('w')
 
 
 def wareShipLineButton():
-	homeTab()
-	pyautogui.press('8')
+    homeTab()
+    pyautogui.press('8')
 
 
 def showWareDocButton():
-	pyautogui.hotkey('shift', 'f7')
+    pyautogui.hotkey('shift', 'f7')
 
 
 def nextButton():
-	pyautogui.hotkey('ctrl', 'pagedown')
+    pyautogui.hotkey('ctrl', 'pagedown')
 
 
 def nextButtonCheck():
-	pos = imagesearch("./nextBlank.png", 1)
-	if pos[0] != -1:
-		return True
-	else:
-		return False
+    pos = imagesearch("./nextBlank.png", 1)
+    if pos[0] != -1:
+        return True
+    else:
+        return False
 
 
 def createPickButton():
-	homeTab()
-	pyautogui.press('k')
+    homeTab()
+    pyautogui.press('k')
 
 
 def pickLinesButton():
-	homeTab()
-	pyautogui.press('s')
+    homeTab()
+    pyautogui.press('s')
 
 
 def cardButton():
-	homeTab()
-	pyautogui.press('c')
+    homeTab()
+    pyautogui.press('c')
 
 
 def quantityButton():
-	homeTab()
-	pyautogui.press('a')
+    homeTab()
+    pyautogui.press('a')
 
 
 def registerButton():
-	homeTab()
-	pyautogui.press('r')
+    homeTab()
+    pyautogui.press('r')
 
 
-#------------------------START OF SCRIPT------------------------
-while outerLoop == 1:
-	sleep(0.3)
-	releaseButton()
-	createWareShipButton()
+if __name__ == "__main__":
+    while True:
+        sleep(0.3)
+        releaseButton()
+        createWareShipButton()
+        sleep(1)
+        if lookImageCheck("./releaseError.png"):
+            pyautogui.press('enter')
+        else:
+            pyautogui.press('enter')
+            innerLoop = True
 
-	sleep(1)
-	if lookImageCheck("./releaseError.png"):
-		pyautogui.press('enter')
-	else:
-		pyautogui.press('enter')
-		innerLoop = 0
-
-	if innerLoop == 1:
-		counter += 1
-		if counter > 10:
-			exit()
-		wareShipLineButton()
-		waitForWindow('Whse. Shipment')
-		showWareDocButton()
-		waitForWindow('Warehouse Shipment')
-		if lookImageCheck("./completelyPicked.png"):
-			pyautogui.press('esc')
-			sleep(0.5)
-			pyautogui.press('esc')
-		sleep(1)
-		if nextButtonCheck:
-			exit()
-		else:
-			nextButton()
-	else:
-		counter = 0
-		innerLoop = 1
-
-		waitForWindow('Warehouse Shipment')
-		print('about to create pick')
-		createPickButton()
-		sleep(1)
-		pyautogui.hotkey('ctrl', 'enter')
-		sleep(1)
-		pyautogui.press('enter')
-		pickLinesButton()
-		waitForWindow('Warehouse Activity')
-		cardButton()
-		waitForWindow('Warehouse Pick')
-		quantityButton()
-		registerButton()
-		sleep(1)
-		if lookImageCheck("./yesButton.png"):
-			pyautogui.press('left')
-			sleep(timeInterval)
-			pyautogui.press('enter')
-			sleep(timeInterval)
-		elif lookImageCheck("./noBin.png"):
-			a = keyboard.read_key('d')
-			if a == 'a':
-				sleep(sleepTime)
-				pyautogui.press('esc')
-				sleep(sleepTime)
-				pyautogui.press('esc')
-			elif a != 'd' and a != 'a':
-				exit()
-		sleep(1)
-		pyautogui.press('esc')
-		sleep(0.5)
-		pyautogui.press('esc')
-		sleep(1)
-		if nextButtonCheck:
-			exit()
-		else:
-			nextButton()
+        if not innerLoop:
+            counter += 1
+            if counter > 10:
+                exit()
+            wareShipLineButton()
+            waitForWindow('Whse. Shipment')
+            showWareDocButton()
+            waitForWindow('Warehouse Shipment')
+            if lookImageCheck("./completelyPicked.png"):
+                pyautogui.press('esc')
+                sleep(0.5)
+                pyautogui.press('esc')
+            sleep(1)
+            if nextButtonCheck:
+                exit()
+            else:
+                nextButton()
+        else:
+            counter = 0
+            innerLoop = False
+            waitForWindow('Warehouse Shipment')
+            print('about to create pick')
+            createPickButton()
+            sleep(1)
+            pyautogui.hotkey('ctrl', 'enter')
+            sleep(1)
+            pyautogui.press('enter')
+            pickLinesButton()
+            waitForWindow('Warehouse Activity')
+            cardButton()
+            waitForWindow('Warehouse Pick')
+            quantityButton()
+            registerButton()
+            sleep(1)
+            if lookImageCheck("./yesButton.png"):
+                pyautogui.press('left')
+                sleep(timeInterval)
+                pyautogui.press('enter')
+                sleep(timeInterval)
+            elif lookImageCheck("./noBin.png"):
+                a = keyboard.read_key('d')
+                if a == 'a':
+                    sleep(sleepTime)
+                    pyautogui.press('esc')
+                    sleep(sleepTime)
+                    pyautogui.press('esc')
+                elif a != 'd' and a != 'a':
+                    exit()
+            sleep(1)
+            pyautogui.press('esc')
+            sleep(0.5)
+            pyautogui.press('esc')
+            sleep(1)
+            if nextButtonCheck:
+                exit()
+            else:
+                nextButton()
